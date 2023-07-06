7fc04c6d57997b94a618e8fd45a77c7a159d674c
KuntimaKiala <kuntima.kiala@gmail.com>
2023-07-06 17:31:13 +0200

    population size fixed to 100

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if __name__ == "__main__" :
     population_size=100
     mutationRate = 0.05
     target_phrase = "Don't talk about my moms yo!."
+    target_phrase = "Just remember ALL CAPS when you spell the man name..."
     init_score = 0.15
     population = initialize(population_size=population_size, 
                             mutationRate=mutationRate, 
